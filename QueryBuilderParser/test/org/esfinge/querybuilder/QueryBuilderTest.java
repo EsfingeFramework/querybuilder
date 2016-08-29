71ff9df50adadde15749c172e3cbf16e95bbd044
Marco Nardes <marconardes@gmail.com>
2016-08-29 11:09:57 -0300

    Revert "Revert "fixing tests""
    This reverts commit 85530b8938dac29dc5cd80d861932220063f730e.

--- QueryBuilderParser/test/org/esfinge/querybuilder/QueryBuilderTest.java
+++ QueryBuilderParser/test/org/esfinge/querybuilder/QueryBuilderTest.java
@@ -21,57 +21,29 @@ import org.junit.runner.RunWith;
 @RunWith(JMock.class)
 public class QueryBuilderTest {
 	
-	protected Mockery context = new Mockery(){{
-        setImposteriser(ClassImposteriser.INSTANCE);
-    }};
+	protected Mockery context = new Mockery() {
+		{ setImposteriser(ClassImposteriser.INSTANCE); }
+	};
 	
-	public interface TestQueries extends Repository<Person>{
+	public interface TestQueries extends Repository<Person> {
 		public Person getPersonByName(String param);
 	}
 	
-	
 	@After
-	public void removeConfigurations(){
+	public void removeConfigurations() {
 		QueryBuilder.configureMethodParser(null);
 		QueryBuilder.configureQueryExecutor(null);
 		QueryBuilder.clearCache();
 	}
 	
 	@Test
-	public void createBasicProxy(){
+	public void createBasicProxy() {
 		Object obj = QueryBuilder.create(TestQueries.class);
 		assertTrue("The return should be assignable to the interface", obj instanceof TestQueries);
 	}
 	
 	@Test
-	public void executeProxyMethod() throws Exception{
-		
-		final DSLMethodParser mp = context.mock(DSLMethodParser.class);
-		final QueryExecutor qe = context.mock(QueryExecutor.class);
-		
-		QueryBuilder.configureMethodParser(mp);
-		QueryBuilder.configureQueryExecutor(qe);
-		
-		TestQueries tq = QueryBuilder.create(TestQueries.class);
-		
-		final Method m = TestQueries.class.getMethod("getPersonByName", String.class);
-		final QueryInfo qi = new QueryInfo();
-		
-		context.checking(new Expectations() {{
-    	 	one(mp).parse(m);
-    		will(returnValue(qi));
-    		one(qe).executeQuery(qi, new Object[]{"Pedro"});
-    		will(returnValue(new Person("Pedro")));
-    	}});
-		
-		Person p = tq.getPersonByName("Pedro");
-		assertEquals("The name should be the same returned by QueryExecutor", "Pedro", p.getName());
-	}
-
-	@Test
-	public void cachedQueryInfo() throws Exception{
-		
-		
+	public void executeProxyMethod() throws Exception {
 		final DSLMethodParser mp = context.mock(DSLMethodParser.class);
 		final QueryExecutor qe = context.mock(QueryExecutor.class);
 		
@@ -84,23 +56,52 @@ public class QueryBuilderTest {
 		final Method m = TestQueries.class.getMethod("getPersonByName", String.class);
 		final QueryInfo qi = new QueryInfo();
 		
-		context.checking(new Expectations() {{
-    	 	one(mp).parse(m);
-    		will(returnValue(qi));
-    		one(qe).executeQuery(qi, new Object[]{"Pedro"});
-    		will(returnValue(new Person("Pedro")));
-    		one(qe).executeQuery(qi, new Object[]{"Jo„o"});
-    		will(returnValue(new Person("Jo„o")));
-    	}});
+		context.checking(new Expectations() {
+			{
+				one(mp).parse(m);
+				will(returnValue(qi));
+				one(qe).executeQuery(qi, new Object[]{"Pedro"});
+				will(returnValue(new Person("Pedro")));
+			}
+		});
+		
+		Person p = tq.getPersonByName("Pedro");
+		assertEquals("The name should be the same returned by QueryExecutor", "Pedro", p.getName());
+	}
+
+	@Test
+	public void cachedQueryInfo() throws Exception {
+		final DSLMethodParser mp = context.mock(DSLMethodParser.class);
+		final QueryExecutor qe = context.mock(QueryExecutor.class);
+		
+		QueryBuilder.clearQueryInfoCache();
+		QueryBuilder.configureMethodParser(mp);
+		QueryBuilder.configureQueryExecutor(qe);
+		
+		TestQueries tq = QueryBuilder.create(TestQueries.class);
+		
+		final Method m = TestQueries.class.getMethod("getPersonByName", String.class);
+		final QueryInfo qi = new QueryInfo();
+		
+		context.checking(new Expectations() {
+			{
+				one(mp).parse(m);
+				will(returnValue(qi));
+				one(qe).executeQuery(qi, new Object[]{"Pedro"});
+				will(returnValue(new Person("Pedro")));
+				one(qe).executeQuery(qi, new Object[]{"Jo√£o"});
+				will(returnValue(new Person("Jo√£o")));
+			}
+		});
 		
 		Person p1 = tq.getPersonByName("Pedro");
 		assertEquals("The name should be the same returned by QueryExecutor", "Pedro", p1.getName());
-		Person p2 = tq.getPersonByName("Jo„o");
-		assertEquals("The name should be the same returned by QueryExecutor", "Jo„o", p2.getName());
+		Person p2 = tq.getPersonByName("Jo√£o");
+		assertEquals("The name should be the same returned by QueryExecutor", "Jo√£o", p2.getName());
 	}
 	
 	@Test
-	public void cachedProxyInstance() throws Exception{
+	public void cachedProxyInstance() throws Exception {
 		TestQueries tq1 = QueryBuilder.create(TestQueries.class);
 		TestQueries tq2 = QueryBuilder.create(TestQueries.class);
 		
@@ -108,7 +109,7 @@ public class QueryBuilderTest {
 	}
 	
 	@Test
-	public void serviceLoaderRetrieval() throws Exception{
+	public void serviceLoaderRetrieval() throws Exception {
 		QueryExecutor qe = QueryBuilder.getConfiguredQueryExecutor();
 		SelectorMethodParser mp = (SelectorMethodParser) QueryBuilder.getConfiguredMethodParser(this.getClass());
 				
@@ -117,7 +118,7 @@ public class QueryBuilderTest {
 	}
 	
 	@Test
-	public void invokeRepositoryMethods(){
+	public void invokeRepositoryMethods() {
 		TestQueries tq = QueryBuilder.create(TestQueries.class);
 		tq.save(new Person());
 		assertEquals("save", DummyRepository.instance.getLastMethodCalled());
