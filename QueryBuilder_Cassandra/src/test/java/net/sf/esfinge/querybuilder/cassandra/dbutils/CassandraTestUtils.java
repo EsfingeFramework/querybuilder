c8088c87b7e4245d7fec57a606f2ee4999a3a8ff
samdalvai <samuel.dalvai@gmail.com>
2022-07-07 21:07:21 +0200

    Speed up cassandra test utils

--- QueryBuilder_Cassandra/src/test/java/net/sf/esfinge/querybuilder/cassandra/dbutils/CassandraTestUtils.java
+++ QueryBuilder_Cassandra/src/test/java/net/sf/esfinge/querybuilder/cassandra/dbutils/CassandraTestUtils.java
@@ -3,21 +3,16 @@ package net.sf.esfinge.querybuilder.cassandra.dbutils;
 
 import com.datastax.driver.core.Session;
 import net.sf.esfinge.querybuilder.cassandra.keyspace.KeyspaceRepository;
-import net.sf.esfinge.querybuilder.cassandra.keyspace.ReplicationStrategy;
-import net.sf.esfinge.querybuilder.cassandra.testresources.Person;
 
 public class CassandraTestUtils {
-
-    private static final String TABLE_NAME = "person";
-    private static final String KEYSPACE_NAME = "test";
 
     public static void initDB() {
         TestCassandraSessionProvider client = new TestCassandraSessionProvider();
         client.connect();
         Session session = client.getSession();
-        KeyspaceRepository schemaRepository = new KeyspaceRepository(session);
 
-        schemaRepository.createKeyspace(KEYSPACE_NAME, ReplicationStrategy.SimpleStrategy, 1);
+        String query = "CREATE KEYSPACE IF NOT EXISTS test WITH replication = {'class':'SimpleStrategy','replication_factor':1};";
+        session.execute(query);
 
         createTables(session);
 
@@ -25,31 +20,7 @@ public class CassandraTestUtils {
     }
 
     public static void createTables(Session session) {
-        StringBuilder sb = new StringBuilder("CREATE TABLE IF NOT EXISTS ")
-                .append(KEYSPACE_NAME + "." + TABLE_NAME)
-                .append("(")
-                .append("id int PRIMARY KEY, ")
-                .append("name text,")
-                .append("lastname text,")
-                .append("age int);");
-
-        final String query = sb.toString();
-
-        session.execute(query);
-    }
-
-    public static void insertPerson(Person person, Session session) {
-        StringBuilder sb = new StringBuilder("INSERT INTO ")
-                .append(KEYSPACE_NAME + "." + TABLE_NAME)
-                .append("(id, name, lastname, age) ")
-                .append("VALUES (")
-                .append(person.getId())
-                .append(", '")
-                .append(person.getName()).append("', '")
-                .append(person.getLastName()).append("', ")
-                .append(person.getAge()).append(");");
-
-        final String query = sb.toString();
+        String query = "CREATE TABLE IF NOT EXISTS test.person(id int PRIMARY KEY, name text,lastname text, age int);";
 
         session.execute(query);
     }
@@ -59,22 +30,12 @@ public class CassandraTestUtils {
         client.connect();
         Session session = client.getSession();
 
-        Person person1 = new Person();
-        person1.setId(1);
-        person1.setName("Homer");
-        person1.setLastName("Simpson");
-        person1.setAge(48);
+        String query = "BEGIN BATCH\n" +
+                "        INSERT INTO test.person(id, name, lastname, age) VALUES (1, 'Homer', 'Simpson', 48);\n" +
+                "        INSERT INTO test.person(id, name, lastname, age) VALUES (2, 'Max', 'Power', 50);\n" +
+                "        APPLY BATCH";
 
-        Person person2 = new Person();
-        person2.setId(2);
-        person2.setName("Max");
-        person2.setLastName("Power");
-        person2.setAge(50);
-
-        insertPerson(person1, session);
-        insertPerson(person2, session);
-
-        client.close();
+        session.execute(query);
     }
 
     public static void cleanTables() {
@@ -82,10 +43,7 @@ public class CassandraTestUtils {
         client.connect();
         Session session = client.getSession();
 
-        StringBuilder sb = new StringBuilder("TRUNCATE ")
-                        .append(KEYSPACE_NAME + "." + TABLE_NAME);
-
-        final String query = sb.toString();
+        String query = "TRUNCATE test.person";
 
         session.execute(query);
     }
@@ -97,7 +55,8 @@ public class CassandraTestUtils {
         Session session = client.getSession();
         KeyspaceRepository schemaRepository = new KeyspaceRepository(session);
 
-        schemaRepository.deleteKeyspace(KEYSPACE_NAME);
-        client.close();
+        String query = "DROP KEYSPACE IF EXISTS test";
+
+        session.execute(query);
     }
 }
