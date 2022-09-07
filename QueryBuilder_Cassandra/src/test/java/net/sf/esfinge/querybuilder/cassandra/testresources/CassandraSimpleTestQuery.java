06e55016e8da5ec24a8d6ef96d46ac145c1b5f8b
samdalvai <samuel.dalvai@gmail.com>
2022-09-07 22:36:37 +0200

    Remove unused test resources

--- QueryBuilder_Cassandra/src/test/java/net/sf/esfinge/querybuilder/cassandra/testresources/CassandraSimpleTestQuery.java
+++ QueryBuilder_Cassandra/src/test/java/net/sf/esfinge/querybuilder/cassandra/testresources/CassandraSimpleTestQuery.java
@@ -9,26 +9,4 @@ public interface CassandraSimpleTestQuery extends Repository<Person> {
 
     List<Person> getPerson();
 
-    Person getPersonById(Integer id);
-
-    List<Person> getPersonByAgeGreater(Integer age);
-
-    Person getPersonByAgeLesserOrEquals(Integer age);
-
-    List<Person> getPersonByLastName(String lastname);
-
-    Person getPersonByNameAndLastName(String name, String lastname);
-
-    List<Person> getPersonByAge(@Greater Integer age);
-
-    List<Person> getPersonByAgeLesser(Integer age);
-
-    List<Person> getPersonByIdAndNameAndLastNameAndAge(Integer id, String name, String lastname, Integer age);
-
-    // QUERIES WITH WRONG NAMING CONVENTION
-    List<Person> getPersonByIdAndNameAndLastName(Integer id, String name);
-
-    List<Person> getPersonByIdAndName(Integer id, String name, Integer age);
-
-
 }
