8618d0d2e8b8c70d0fd7fadd3afd3d7e6c2d0547
Fernando de Oliveira Pereira <fernando.pereira@cemaden.gov.br>
2024-03-26 15:48:12 -0300

    upgrade to java 17

--- QueryBuilder_JPA1/src/main/java/net/sf/esfinge/querybuilder/jpa1/JPAEntityClassProvider.java
+++ QueryBuilder_JPA1/src/main/java/net/sf/esfinge/querybuilder/jpa1/JPAEntityClassProvider.java
@@ -2,22 +2,23 @@ package net.sf.esfinge.querybuilder.jpa1;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.metamodel.EntityType;
+
+import net.sf.esfinge.querybuilder.exception.EntityClassNotFoundException;
 import net.sf.esfinge.querybuilder.methodparser.EntityClassProvider;
 import net.sf.esfinge.querybuilder.utils.ServiceLocator;
 
 public class JPAEntityClassProvider implements EntityClassProvider {
 
-    @Override
-    public Class<?> getEntityClass(String name) {
-        EntityManagerProvider emp = ServiceLocator.getServiceImplementation(EntityManagerProvider.class);
-        EntityManagerFactory emf = emp.getEntityManagerFactory();
-        for (EntityType et : emf.getMetamodel().getEntities()) {
-            String entityName = et.getName().substring(et.getName().lastIndexOf(".") + 1);
-            if (entityName.equalsIgnoreCase(name)) {
-                return et.getJavaType();
-            }
-        }
-        return null;
-    }
+	@Override
+	public Class<?> getEntityClass(String name) {
+		EntityManagerProvider emp = ServiceLocator.getServiceImplementation(EntityManagerProvider.class);
+		EntityManagerFactory emf = emp.getEntityManagerFactory();
+		for (EntityType et : emf.getMetamodel().getEntities()){
+			String entityName = et.getName().substring(et.getName().lastIndexOf(".")+1);
+			if(entityName.equalsIgnoreCase(name))
+				return et.getJavaType();
+		}
+		return null;
+	}
 
 }
--- QueryBuilder_JPA1/src/test/java/net/sf/esfinge/querybuilder/jpa1/TestJPAEntityClassProvider.java
+++ QueryBuilder_JPA1/src/test/java/net/sf/esfinge/querybuilder/jpa1/TestJPAEntityClassProvider.java
@@ -1,23 +1,27 @@
 package net.sf.esfinge.querybuilder.jpa1;
 
-import net.sf.esfinge.querybuilder.jpa1.testresources.Address;
-import net.sf.esfinge.querybuilder.jpa1.testresources.Person;
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
+import net.sf.esfinge.querybuilder.exception.EntityClassNotFoundException;
+import net.sf.esfinge.querybuilder.jpa1.JPAEntityClassProvider;
+import net.sf.esfinge.querybuilder.jpa1.testresources.Address;
+import net.sf.esfinge.querybuilder.jpa1.testresources.Person;
+
 public class TestJPAEntityClassProvider {
-
-    @Test
-    public void getEntityClass() {
-        JPAEntityClassProvider provider = new JPAEntityClassProvider();
-        assertEquals("Should retrieve Person", Person.class, provider.getEntityClass("Person"));
-        assertEquals("Should retrieve Address", Address.class, provider.getEntityClass("Address"));
-    }
-
-    @Test
-    public void entityClassNotFound() {
-        JPAEntityClassProvider provider = new JPAEntityClassProvider();
-        assertNull(provider.getEntityClass("Other"));
-    }
+	
+	@Test
+	public void getEntityClass(){
+		JPAEntityClassProvider provider = new JPAEntityClassProvider();
+		assertEquals("Should retrieve Person", Person.class, provider.getEntityClass("Person"));
+		assertEquals("Should retrieve Address", Address.class, provider.getEntityClass("Address"));
+	}
+	
+	@Test
+	public void entityClassNotFound(){
+		JPAEntityClassProvider provider = new JPAEntityClassProvider();
+		assertNull(provider.getEntityClass("Other"));
+	}
 
 }
