81cb440d2cd529b7377069b4c2748a115afe3da6
Fernando de Oliveira Pereira <fernando.pereira@cemaden.gov.br>
2024-11-21 02:23:22 -0300

    refactoring Cassandra

--- QueryBuilder_Cassandra/src/main/java/ef/qb/cassandra/entity/CassandraEntity.java
+++ QueryBuilder_Cassandra/src/main/java/ef/qb/cassandra/entity/CassandraEntity.java
@@ -2,7 +2,7 @@ package ef.qb.cassandra.entity;
 
 /**
  * Empty interface uses to signal that a particular Class has to be viewed as an entity for tha cassandra database Used
- * by the {@link CassandraEntityClassProvider#getEntityClass(String s)} method
+ * by the {@link ef.qb.cassandra.CassandraEntityClassProvider#getEntityClass(String s)} method
  */
 public interface CassandraEntity {
 }
